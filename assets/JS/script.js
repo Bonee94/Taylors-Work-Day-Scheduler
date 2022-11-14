e987c9e818f6372e9ec8a6aee33a0167413837b6
Bonee94 <bonee94@yahoo.com>
2022-11-13 20:55:10 -0600

    added hour writing function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,32 @@
-let currentDayDisplay = document.getElementById('currentDay')
-
+let currentDayDisplay = document.getElementById('currentDay');
+let hoursContainer = document.getElementById('hoursList');
 let thisDay = moment().format('LL');
 
-currentDayDisplay.textContent = thisDay
+// Time prep for checking hours
+let timeIncoming = moment.utc();
+timeIncoming.local();
+let thisHour = timeIncoming.hours();
+currentDayDisplay.textContent = thisDay;
+console.log(thisHour)
+
+
+let hoursAvailable = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+console.log(hoursAvailable);
+
+
+
+function writeHours() {
+    
+    for (let i = 0; i < hoursAvailable.length; i++) {
+        //  writes Elements
+        let divEl = document.createElement('div');
+        let divEl2 = document.createElement('div');
+        let textArea = document.createElement('textarea');
+        let buttonEl = document.createElement('button');
+        let iconEl = document.createElement('i');
+
+    }
+
+}
+
+writeHours()
